e827b2e7d0ff3ee00305ded2b3286f8e52a468d5
Soumyajit Maity <soumyajitmaity829@gmail.com>
2026-06-12 13:18:26 +0530

    updated code

--- train/train.py
+++ train/train.py
@@ -5,32 +5,37 @@ from torchvision import datasets
 from torchvision import transforms
 from torchvision.models import efficientnet_b0
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+device = torch.device("cuda")
+
+print("Using device:", device)
 
 transform = transforms.Compose([
-    transforms.Resize((224,224)),
+    transforms.Resize((224, 224)),
     transforms.ToTensor()
 ])
 
 train_dataset = datasets.ImageFolder(
-    "dataset/train",
+    "dataset/chest_xray/train",
     transform=transform
 )
 
 val_dataset = datasets.ImageFolder(
-    "dataset/val",
+    "dataset/chest_xray/val",
     transform=transform
 )
 
 train_loader = torch.utils.data.DataLoader(
     train_dataset,
-    batch_size=32,
-    shuffle=True
+    batch_size=64,
+    shuffle=True,
+    num_workers=4
 )
 
 val_loader = torch.utils.data.DataLoader(
     val_dataset,
-    batch_size=32
+    batch_size=64,
+    shuffle=False,
+    num_workers=4
 )
 
 model = efficientnet_b0(weights="DEFAULT")
@@ -49,11 +54,15 @@ optimizer = torch.optim.Adam(
     lr=0.001
 )
 
+best_acc = 0
+
 epochs = 5
 
 for epoch in range(epochs):
 
     model.train()
+
+    running_loss = 0
 
     for images, labels in train_loader:
 
@@ -70,11 +79,45 @@ for epoch in range(epochs):
 
         optimizer.step()
 
-    print(f"Epoch {epoch+1} completed")
+        running_loss += loss.item()
 
-torch.save(
-    model.state_dict(),
-    "../backend/best_model.pth"
-)
+    model.eval()
 
-print("Model saved")
+    correct = 0
+    total = 0
+
+    with torch.no_grad():
+
+        for images, labels in val_loader:
+
+            images = images.to(device)
+            labels = labels.to(device)
+
+            outputs = model(images)
+
+            _, predicted = torch.max(outputs, 1)
+
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+
+    accuracy = 100 * correct / total
+
+    print(
+        f"Epoch {epoch+1}/{epochs} "
+        f"Loss={running_loss:.4f} "
+        f"ValAcc={accuracy:.2f}%"
+    )
+
+    if accuracy > best_acc:
+
+        best_acc = accuracy
+
+        torch.save(
+            model.state_dict(),
+            "../backend/models/best_model.pth"
+        )
+
+        print("Model Saved")
+
+print("Training Complete")
+print("Best Accuracy:", best_acc)
