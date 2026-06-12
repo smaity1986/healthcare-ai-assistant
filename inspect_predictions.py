2716a1fe01a35cf1a6c3d3a7c4bafc615aea39af
Soumyajit Maity <soumyajitmaity829@gmail.com>
2026-06-12 14:19:13 +0530

    Added source code

--- inspect_predictions.py
+++ inspect_predictions.py
@@ -6,7 +6,7 @@ from backend.predict import predict_image
 print("Testing NORMAL image...")
 
 img = Image.open(
-    "train/dataset/chest_xray/test/NORMAL/IM-0001-0001.jpeg"
+    "dataset/chest_xray/test/NORMAL/IM-0001-0001.jpeg"
 ).convert("RGB")
 
 print(predict_image(img))
@@ -14,7 +14,7 @@ print(predict_image(img))
 print("\nTesting PNEUMONIA image...")
 
 img = Image.open(
-    "train/dataset/chest_xray/test/PNEUMONIA/person1_virus_6.jpeg"
+    "dataset/chest_xray/test/PNEUMONIA/person1_virus_6.jpeg"
 ).convert("RGB")
 
 print(predict_image(img))
