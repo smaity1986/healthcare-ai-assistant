a74d980a5233b5b719aea6905cbd0044f11a388c
Soumyajit Maity <soumyajitmaity829@gmail.com>
2026-06-12 13:31:55 +0530

    Added source code

---
+++ train/confusion_matrix.py
@@ -0,0 +1,57 @@
+import torch
+import torch.nn as nn
+
+from torchvision import datasets
+from torchvision import transforms
+from torchvision.models import efficientnet_b0
+
+device = "cuda"
+
+transform = transforms.Compose([
+    transforms.Resize((224,224)),
+    transforms.ToTensor()
+])
+
+dataset = datasets.ImageFolder(
+    "dataset/chest_xray/test",
+    transform=transform
+)
+
+loader = torch.utils.data.DataLoader(
+    dataset,
+    batch_size=32,
+    shuffle=False
+)
+
+model = efficientnet_b0(weights=None)
+model.classifier[1] = nn.Linear(1280, 2)
+
+model.load_state_dict(
+    torch.load(
+        "backend/models/best_model.pth",
+        map_location=device
+    )
+)
+
+model = model.to(device)
+model.eval()
+
+cm = [[0,0],[0,0]]
+
+with torch.no_grad():
+
+    for images, labels in loader:
+
+        images = images.to(device)
+
+        outputs = model(images)
+
+        preds = outputs.argmax(dim=1).cpu()
+
+        for p, l in zip(preds, labels):
+            cm[l][p] += 1
+
+print("Classes:", dataset.classes)
+print()
+print("Confusion Matrix")
+print(cm)
